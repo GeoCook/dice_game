58460fcfdffd2cb067c4d29c890b674768c7f2c1
CN74 <cn74@CN74s-MacBook-Pro.local>
2019-09-18 12:20:33 +0100

    Updates

---
+++ precache-manifest.dbe4924f7d747e16be039efdec00c6f6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90e9c67465c96b6d445e2d3243c7e161",
+    "url": "/dice_game/index.html"
+  },
+  {
+    "revision": "fd0b5a4d49ed6d7dd414",
+    "url": "/dice_game/static/css/main.d1e31d2d.chunk.css"
+  },
+  {
+    "revision": "5f54086f7f42f92ed5fa",
+    "url": "/dice_game/static/js/2.1b11c675.chunk.js"
+  },
+  {
+    "revision": "fd0b5a4d49ed6d7dd414",
+    "url": "/dice_game/static/js/main.6b460f77.chunk.js"
+  },
+  {
+    "revision": "551bfb8cd19060a026da",
+    "url": "/dice_game/static/js/runtime~main.492dc205.js"
+  },
+  {
+    "revision": "e5095c414eed9af1dcf6f80ea12b45eb",
+    "url": "/dice_game/static/media/button.e5095c41.png"
+  },
+  {
+    "revision": "cdbf4da4a77ea31698aae46925fde41a",
+    "url": "/dice_game/static/media/face000.cdbf4da4.png"
+  },
+  {
+    "revision": "166be8040fa7d61b630ec1607f2311a0",
+    "url": "/dice_game/static/media/face001.166be804.png"
+  },
+  {
+    "revision": "090aec2ddc51bc8624541cdaa8245274",
+    "url": "/dice_game/static/media/face002.090aec2d.png"
+  },
+  {
+    "revision": "ef9d2659d10180c239f0b63afff959f2",
+    "url": "/dice_game/static/media/face003.ef9d2659.png"
+  },
+  {
+    "revision": "a063cb200edc02404988ef4efc0d3ef7",
+    "url": "/dice_game/static/media/face004.a063cb20.png"
+  },
+  {
+    "revision": "7107b41a2ee8f7a21cfed630b1adf5a3",
+    "url": "/dice_game/static/media/face005.7107b41a.png"
+  },
+  {
+    "revision": "ccb29bfc06369cc903422a66e77af0ef",
+    "url": "/dice_game/static/media/face006.ccb29bfc.png"
+  }
+]);
